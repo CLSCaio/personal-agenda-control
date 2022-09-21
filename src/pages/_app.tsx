4aafbba7206803e2b5c9c7573718ebae696526bc
Caio Sousa <caiodelimack@hotmail.com>
2022-09-21 14:59:47 -0300

    refactor: ajustando tela de login e app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { GlobalStyle, Main } from "library-caiol.sousa";
 
 import { Root } from "../layouts";
+import { getCookie, IGlobalAuth } from "../auth";
+import { Login } from "../views";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   const queryClient = new QueryClient();
+  const [cookie, setCookie] = useState<IGlobalAuth | undefined>(undefined);
+
+  useEffect(() => {
+    const cookies = getCookie();
+    setCookie(cookies);
+  }, []);
 
   return (
     <QueryClientProvider client={queryClient}>
-      <Root>
-        <Component {...pageProps} />
-      </Root>
+      {!cookie ? (
+        <Main>
+          <Login />
+        </Main>
+      ) : (
+        <Root>
+          <Component {...pageProps} />
+        </Root>
+      )}
+      <GlobalStyle />
     </QueryClientProvider>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Container } from "library-caiol.sousa";
 
-import { getCookie, IGlobalAuth } from "../auth";
+import { Home } from "../views";
 
-import { Home, Login } from "../views";
-
-const Base: React.FC = () => {
-  const cookies = getCookie();
-
-  return (
-    <Container gap={[100, 30]} direction="column">
-      {!cookies ? <Login /> : <Home />}
-    </Container>
-  );
-};
+const Base: React.FC = () => (
+  <Container gap={[100, 30]} direction="column">
+    <Home />
+  </Container>
+);
 
 export default Base;
